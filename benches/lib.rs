8df847e39a18b5b19431236cbe09a873b77964d9
Xudong Huang <huangxu008@hotmail.com>
2024-11-08 21:09:23 +0800

    :pencil: add multi thread bench

--- benches/lib.rs
+++ benches/lib.rs
@@ -45,3 +45,49 @@ fn read_write_1(b: &mut Bencher) {
         assert_eq!(REF.load(Ordering::Relaxed), 1001);
     });
 }
+
+#[bench]
+fn read_write_2(b: &mut Bencher) {
+    static REF: AtomicUsize = AtomicUsize::new(0);
+
+    struct Foo(usize);
+
+    impl Drop for Foo {
+        fn drop(&mut self) {
+            REF.fetch_add(1, Ordering::Relaxed);
+        }
+    }
+
+    b.iter(|| {
+        REF.store(0, Ordering::Relaxed);
+        let rcu_cell = Arc::new(RcuCell::new(Foo(42)));
+        std::thread::scope(|s| {
+            let rcu = rcu_cell.clone();
+            s.spawn(move || {
+                for i in 0..1000 {
+                    rcu.write(Foo(i));
+                }
+            });
+
+            let rcu = rcu_cell.clone();
+            s.spawn(move || {
+                for i in 0..1000 {
+                    rcu.write(Foo(i));
+                }
+            });
+
+            let readers = 8;
+            for _ in 0..readers {
+                let rcu = rcu_cell.clone();
+                s.spawn(move || {
+                    for _i in 0..1000 {
+                        let _v = rcu.read().unwrap();
+                    }
+                });
+            }
+        });
+        assert_eq!(rcu_cell.read().unwrap().0, 999);
+        drop(rcu_cell);
+        assert_eq!(REF.load(Ordering::Relaxed), 2001);
+    });
+}
