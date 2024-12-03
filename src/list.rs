1b0fdd8c0f2d29022f095755bb2ff641eac2d548
Xudong-Huang <huangxu008@hotmail.com>
2024-12-03 18:02:36 +0800

    :pencil: list impl

--- src/list.rs
+++ src/list.rs
@@ -1,8 +1,7 @@
 use alloc::sync::{Arc, Weak};
 use core::marker::PhantomData;
 use core::ops::Deref;
-use core::sync::atomic::AtomicUsize;
-use core::sync::atomic::Ordering;
+use core::sync::atomic::{AtomicUsize, Ordering};
 
 use crate::RcuCell;
 
@@ -23,20 +22,44 @@ impl<T> Node<T> {
         }
     }
 
-    fn try_lock(&self) -> bool {
-        self.version
-            .compare_exchange(0, 1, Ordering::Relaxed, Ordering::Relaxed)
-            .is_ok()
-    }
-
-    fn lock(&self) {
-        while !self.try_lock() {
-            core::hint::spin_loop();
+    fn try_lock(&self) -> Result<usize, ()> {
+        let version = self.version.load(Ordering::Relaxed);
+        if version & 1 == 1 {
+            return Err(());
+        }
+        match self.version.compare_exchange_weak(
+            version,
+            version + 2,
+            Ordering::Relaxed,
+            Ordering::Relaxed,
+        ) {
+            Ok(_) => Ok(version),
+            Err(_) => Ok(usize::MAX),
         }
     }
 
+    fn lock(&self) -> Result<usize, ()> {
+        let mut version;
+        loop {
+            version = self.try_lock()?;
+            if version != usize::MAX {
+                break;
+            }
+            core::hint::spin_loop();
+        }
+        Ok(version)
+    }
+
     fn unlock(&self) {
-        self.version.store(0, Ordering::Relaxed);
+        self.version.fetch_add(2, Ordering::Relaxed);
+    }
+
+    fn unlock_remove(&self) {
+        self.version.fetch_add(3, Ordering::Relaxed);
+    }
+
+    fn is_removed(&self) -> bool {
+        self.version.load(Ordering::Relaxed) & 1 == 1
     }
 }
 
@@ -50,39 +73,49 @@ impl<T> Entry<T> {
     }
 
     /// insert a node after the current node
-    pub fn insert_after(&self, data: T) -> Entry<T> {
-        Entry {
-            node: self.as_impl().insert_after(data),
-        }
+    pub fn insert_after(&self, data: T) -> Result<Entry<T>, T> {
+        Ok(Entry {
+            node: self.as_impl().insert_after(data)?,
+        })
     }
 
     /// insert a node before the current node
-    pub fn insert_ahead(&self, data: T) -> Entry<T> {
-        Entry {
-            node: self.as_impl().insert_ahead(data),
-        }
+    pub fn insert_ahead(&self, data: T) -> Result<Entry<T>, T> {
+        Ok(Entry {
+            node: self.as_impl().insert_ahead(data)?,
+        })
     }
 
     /// remove the node from the list
     pub fn remove(self) {
-        let prev_node = self.as_impl().lock_prev_node();
-        let curr = self.node.read().unwrap().clone();
-        let prev = curr.prev.upgrade().unwrap();
-        let next = curr.next.read().unwrap().clone();
-        curr.lock();
+        let (prev, prev_node) = match self.as_impl().lock_prev_node() {
+            Ok((prev, prev_node)) => (prev, prev_node),
+            Err(_) => return,
+        };
+        let curr_node = self.node.read().unwrap();
+        let next_node = curr_node.next.read().unwrap();
+        if curr_node.lock().is_err() {
+            // current node is removed
+            prev_node.unlock();
+            return;
+        }
+        assert!(Arc::ptr_eq(&prev, &curr_node.prev.upgrade().unwrap()));
+        // update prev.next
         prev.write(Node {
             version: prev_node.version.clone(),
             prev: prev_node.prev.clone(),
-            next: curr.next.clone(),
+            next: curr_node.next.clone(),
             data: prev_node.data.clone(),
         });
-        curr.next.write(Node {
-            version: next.version.clone(),
-            prev: curr.prev.clone(),
-            next: next.next.clone(),
-            data: next.data.clone(),
+        // update next.prev
+        curr_node.next.write(Node {
+            version: next_node.version.clone(),
+            prev: curr_node.prev.clone(),
+            next: next_node.next.clone(),
+            data: next_node.data.clone(),
         });
-        curr.unlock();
+        // make current node removed
+        curr_node.unlock_remove();
         prev_node.unlock();
     }
 }
@@ -91,102 +124,153 @@ pub struct EntryImpl<'a, T>(&'a Arc<RcuCell<Node<T>>>);
 
 impl<'a, T> EntryImpl<'a, T> {
     /// lock the prev node
-    fn lock_prev_node(&self) -> Arc<Node<T>> {
+    fn lock_prev_node(&self) -> Result<(Arc<RcuCell<Node<T>>>, Arc<Node<T>>), ()> {
         loop {
-            let prev = self.0.read().unwrap();
-            prev.lock();
-            if !Arc::ptr_eq(&self.0, &prev.next) {
-                prev.unlock();
+            let curr = self.0.read().unwrap();
+            let prev = match curr.prev.upgrade() {
+                // something wrong, like the prev node is deleted, or the current node is deleted
+                None => {
+                    if curr.is_removed() {
+                        return Err(());
+                    } else {
+                        // try again
+                        continue;
+                    }
+                }
+
+                // the prev can change due to prev insert/remove
+                Some(prev) => prev,
+            };
+            let prev_node = prev.read().unwrap();
+            if prev_node.lock().is_err() {
+                // the prev node is removed
+                continue;
+            }
+            if !Arc::ptr_eq(self.0, &prev_node.next) {
+                // the prev node is changed
+                prev_node.unlock();
                 continue;
             }
             // successfully lock the prev node
-            return prev;
+            return Ok((prev, prev_node));
         }
     }
 
     /// insert a node after the current node
-    fn insert_after(&self, data: T) -> Arc<RcuCell<Node<T>>> {
+    /// this could failed due to the current node is removed
+    fn insert_after(&self, data: T) -> Result<Arc<RcuCell<Node<T>>>, T> {
         // first lock the current node
-        let prev = self.lock_prev_node();
+        let curr_node = self.0.read().unwrap();
+        if curr_node.lock().is_err() {
+            // current node is removed
+            return Err(data);
+        }
+        self.insert_after_locked(curr_node, data)
+    }
+
+    fn insert_after_locked(
+        &self,
+        curr_node: Arc<Node<T>>,
+        data: T,
+    ) -> Result<Arc<RcuCell<Node<T>>>, T> {
+        let next_node = curr_node.next.read().unwrap();
         let new_entry = Arc::new(RcuCell::new(Node {
             version: Arc::new(AtomicUsize::new(0)),
             prev: Arc::downgrade(&self.0),
-            next: prev.next.clone(),
+            next: curr_node.next.clone(),
             data: Arc::new(Some(data)),
         }));
-        // let new_entry = Arc::new(RcuCell::new(Entry::new(elt)));
-        // let new_entry_ref = new_entry.read().unwrap();
-        // new_entry_ref.prev = Arc::downgrade(&self.head);
-        // new_entry_ref.next = next.clone();
-        // next.write(new_entry_ref);
-        // head.next = new_entry;
-        todo!();
+
+        curr_node.next.write(Node {
+            version: next_node.version.clone(),
+            prev: Arc::downgrade(&new_entry),
+            next: next_node.next.clone(),
+            data: next_node.data.clone(),
+        });
+
+        self.0.write(Node {
+            version: curr_node.version.clone(),
+            prev: curr_node.prev.clone(),
+            next: new_entry.clone(),
+            data: curr_node.data.clone(),
+        });
+        curr_node.unlock();
+        Ok(new_entry)
     }
 
     /// insert a node before the current node
-    fn insert_ahead(&self, data: T) -> Arc<RcuCell<Node<T>>> {
+    /// this could failed due to the current node is removed
+    fn insert_ahead(&self, data: T) -> Result<Arc<RcuCell<Node<T>>>, T> {
         // first lock the current node
-        let prev = self.lock_prev_node();
-        let new_entry = Arc::new(RcuCell::new(Node {
-            version: Arc::new(AtomicUsize::new(0)),
-            prev: Arc::downgrade(&self.0),
-            next: prev.next.clone(),
-            data: Arc::new(Some(data)),
-        }));
-
-        todo!()
+        let (prev, prev_node) = match self.lock_prev_node() {
+            Ok((prev, prev_node)) => (prev, prev_node),
+            Err(_) => return Err(data),
+        };
+        let prev = EntryImpl(&prev);
+        prev.insert_after_locked(prev_node, data)
     }
-
 
     /// remove the node before the current node
     /// this could invalidate the prev Entry!!
     fn remove_ahead(&self, head: &Arc<RcuCell<Node<T>>>) -> Option<Arc<RcuCell<Node<T>>>> {
-        // self.0 is the next node
-        loop {
-            let curr = self.lock_prev_node();
-            let prev = match curr.prev.upgrade() {
-                None => {
-                    // this is the head
-                    curr.unlock();
-                    return None;
-                }
-                // the prev can change due to prev insert/remove
-                Some(prev) => prev,
-            };
-            // there could be dead lock, we must first lock prev
-            curr.unlock();
-            let prev = prev.read().unwrap();
-            prev.lock();
+        let (prev, prev_node) = match self.lock_prev_node() {
+            Ok((prev, prev_node)) => (prev, prev_node),
+            Err(_) => return None,
+        };
+        if Arc::ptr_eq(&prev, head) {
+            // we can't remove the head node
+            prev_node.unlock();
+            return None;
         }
+        self.remove_after_locked(prev_node, None)
     }
 
     /// remove the node after the current node
+    /// if the current node is removed, return None
+    /// if the next node is tail, return None
     fn remove_after(&self, tail: &Arc<RcuCell<Node<T>>>) -> Option<Arc<RcuCell<Node<T>>>> {
         // self.0 is the prev node
-        let prev = self.0.read().unwrap();
-        prev.lock();
-        if Arc::ptr_eq(&prev.next, tail) {
-            prev.unlock();
-            return None;
+        let curr_node = self.0.read().unwrap();
+        curr_node.lock().ok()?;
+        self.remove_after_locked(curr_node, Some(tail))
+    }
+
+    fn remove_after_locked(
+        &self,
+        curr_node: Arc<Node<T>>,
+        tail: Option<&Arc<RcuCell<Node<T>>>>,
+    ) -> Option<Arc<RcuCell<Node<T>>>> {
+        // self.0 is the prev node
+        let prev_node = curr_node;
+        if let Some(tail) = tail {
+            if Arc::ptr_eq(&prev_node.next, tail) {
+                // we can't remove the tail node
+                prev_node.unlock();
+                return None;
+            }
         }
-        let curr = prev.next.read().unwrap();
-        curr.lock();
-        let ret = prev.next.clone();
+
+        let curr_node = prev_node.next.read().unwrap();
+        curr_node.lock().unwrap();
+        let ret = prev_node.next.clone();
+        // update prev.next
         self.0.write(Node {
-            version: prev.version.clone(),
-            prev: prev.prev.clone(),
-            next: curr.next.clone(),
-            data: prev.data.clone(),
+            version: prev_node.version.clone(),
+            prev: prev_node.prev.clone(),
+            next: curr_node.next.clone(),
+            data: prev_node.data.clone(),
         });
-        let next = curr.next.read().unwrap();
-        curr.next.write(Node {
-            version: next.version.clone(),
-            prev: curr.prev.clone(),
-            next: next.next.clone(),
-            data: next.data.clone(),
+
+        // update next.prev
+        let next_node = curr_node.next.read().unwrap();
+        curr_node.next.write(Node {
+            version: next_node.version.clone(),
+            prev: curr_node.prev.clone(),
+            next: next_node.next.clone(),
+            data: next_node.data.clone(),
         });
-        curr.unlock();
-        prev.unlock();
+        curr_node.unlock_remove();
+        prev_node.unlock();
         Some(ret)
     }
 }
@@ -270,7 +354,10 @@ impl<T> LinkedList<T> {
 
     pub fn push_back(&mut self, elt: T) -> EntryRef<T> {
         let entry = EntryImpl(&self.tail);
-        let new_entry = entry.insert_ahead(elt);
+        let new_entry = match entry.insert_ahead(elt) {
+            Ok(entry) => entry,
+            Err(_) => panic!("push_back failed"),
+        };
         new_entry.read().map(EntryRef).unwrap()
     }
 
@@ -283,7 +370,10 @@ impl<T> LinkedList<T> {
 
     pub fn push_front(&mut self, elt: T) -> EntryRef<T> {
         let entry = EntryImpl(&self.head);
-        let new_entry = entry.insert_after(elt);
+        let new_entry = match entry.insert_after(elt) {
+            Ok(entry) => entry,
+            Err(_) => panic!("push_front failed"),
+        };
         new_entry.read().map(EntryRef).unwrap()
     }
 
@@ -296,7 +386,6 @@ impl<T> LinkedList<T> {
 
     pub fn iter(&self) -> Iter<T> {
         Iter {
-            head: self.head.clone(),
             tail: self.tail.clone(),
             curr: self.head.clone(),
             marker: PhantomData,
@@ -309,7 +398,6 @@ impl<T> LinkedList<T> {
 /// This `struct` is created by [`LinkedList::iter()`]. See its
 /// documentation for more.
 pub struct Iter<'a, T: 'a> {
-    head: Arc<RcuCell<Node<T>>>,
     tail: Arc<RcuCell<Node<T>>>,
     curr: Arc<RcuCell<Node<T>>>,
     marker: PhantomData<&'a Node<T>>,
@@ -325,19 +413,6 @@ impl<T> Iterator for Iter<'_, T> {
 
         let curr = self.curr.read().unwrap();
         self.curr = curr.next.clone();
-        Some(EntryRef(curr))
-    }
-}
-
-impl<T> DoubleEndedIterator for Iter<'_, T> {
-    fn next_back(&mut self) -> Option<Self::Item> {
-        if Arc::ptr_eq(&self.curr, &self.head) {
-            return None;
-        }
-
-        let curr = self.curr.read().unwrap();
-        // TODO: need to check prev deleted
-        self.curr = curr.prev.upgrade().unwrap();
         Some(EntryRef(curr))
     }
 }
